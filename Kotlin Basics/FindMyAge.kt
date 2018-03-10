1e9f5f7606e74776bdd1d02d814fe1b9bee2e166
vipul018 <vipulsharma018@gmail.com>
2018-03-10 14:01:21 -0500

    Calendar instance usage

--- Kotlin Basics/FindMyAge.kt
+++ Kotlin Basics/FindMyAge.kt
@@ -5,7 +5,9 @@ fun main(args:Array<String>)
     print("Enter your DOB year: ")
     var DOB:Int = readLine()!!.toInt()
     var currentYear = Calendar.getInstance().get(Calendar.YEAR)
-    var age = currentYear - DOB
+    var age:Int?
+
+    age = currentYear - DOB
 
     println("Your age is $age years")
 }
