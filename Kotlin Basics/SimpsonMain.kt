7e935fb351a896a7884f13b7e3463b4b19b9c2fe
vipul018 <vipulsharma018@gmail.com>
2018-03-11 15:44:20 -0400

    Kotlin class Inheritance update

--- Kotlin Basics/SimpsonMain.kt
+++ Kotlin Basics/SimpsonMain.kt
@@ -1,10 +1,30 @@
+import com.sun.org.apache.xml.internal.security.algorithms.implementations.IntegrityHmac
+
 fun makeSimpson() {
     val homer = SimpsonClassDemo(name = "Homer", age = 50, job = "Nuclear Safety")
     homer.age = 51
 
-    print(homer.name + "'s age: " + homer.age)
+    println(homer.name + "'s age: " + homer.age)
 }
 
 fun main(args: Array<String>) {
     makeSimpson();
+    val op = Sum()
+    println("Sum: " + op.sum(3,5))
+    println("Div: " + op.div(6,3))
+}
+
+//By default all the classes are final
+class Sum:Div(){
+    fun sum(a: Int,b: Int):Int
+    {
+     return a + b
+    }
+}
+
+//in-order to make a class inheritable make it open
+open class Div{
+    fun div(a:Int,b:Int):Int{
+        return a/b
+    }
 }
