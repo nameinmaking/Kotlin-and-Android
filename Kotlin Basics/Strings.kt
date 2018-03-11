359fcda3b1a7534cf5394a55508aac47de289eab
vipul018 <vipulsharma018@gmail.com>
2018-03-10 21:19:31 -0500

    String example

---
+++ Kotlin Basics/Strings.kt
@@ -0,0 +1,11 @@
+fun main(args:Array<String>)
+{
+    var title = " Welcome to New York "
+    println("ttile: $title")
+    println("Name: " + "Vipul" + " Sharma")
+    println("2nd Char: " + title[1])
+    println("In lowercase: " + title.toLowerCase())
+    println("In uppercase: " + title.toUpperCase())
+    println("Split result: " + title.split(" "))
+    println("Trimming it: " + title.trim())
+}
